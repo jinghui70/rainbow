31cc957fd2b383dd8a0c5f34ecc751058abcbef4
jinghui70 <jinghui70@gmail.com>
2019-05-05 09:05:57 +0800

    增加delete的两个方便函数

--- db/src/rainbow/db/dao/Delete.java
+++ db/src/rainbow/db/dao/Delete.java
@@ -11,4 +11,12 @@ public class Delete extends Where<Delete> {
 		return dao.execSql(sql);
 	}
 
+	public int byId(String id) {
+		return where("id", id).excute();
+	}
+	
+	public int byKey(Object...objects) {
+		Sql sql = new Sql("DELETE FROM ").append(entity.getCode()).whereKey(entity, objects);
+		return dao.execSql(sql);
+	}
 }
